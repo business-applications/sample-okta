a951274faf8fc0c8667baf042ff29ec0c0d109e5
tsurdilo <tsurdilo@redhat.com>
2018-12-27 12:22:55 -0500

    updating display

--- sample-okta-service/src/main/java/com/company/service/ApplicationController.java
+++ sample-okta-service/src/main/java/com/company/service/ApplicationController.java
@@ -1,8 +1,9 @@
 package com.company.service;
 
-import java.security.Principal;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +13,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ApplicationController {
 
     @GetMapping("/")
-    public String showIndexPage(Model model,
-                                Principal principal) {
+    public String showIndexPage(Model model) {
 
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        String principal = (String) auth.getPrincipal();
+        OAuth2AuthenticationDetails authDetails = (OAuth2AuthenticationDetails) auth.getDetails();
+
+        model.addAttribute("authenticated",
+                           auth.isAuthenticated());
+        model.addAttribute("authdetails",
+                           authDetails);
         model.addAttribute("user",
-                           principal.getName());
+                           principal);
 
         return "index";
     }
